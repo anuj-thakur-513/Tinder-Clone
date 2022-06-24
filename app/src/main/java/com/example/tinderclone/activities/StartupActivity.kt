68e1b88d13e74e08463b98462f944d7573fb2c8d
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-06-24 21:56:58 +0530

    added image resources and removed night theme

---
+++ app/src/main/java/com/example/tinderclone/activities/StartupActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tinderclone.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_startup)
+    }
+}
