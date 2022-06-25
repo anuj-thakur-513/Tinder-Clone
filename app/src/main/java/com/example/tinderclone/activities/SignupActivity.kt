c257e18a334f06079e6c72e1a3275121656f843a
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-06-25 23:26:06 +0530

    creating user in signup activity

--- app/src/main/java/com/example/tinderclone/activities/SignupActivity.kt
+++ app/src/main/java/com/example/tinderclone/activities/SignupActivity.kt
@@ -5,16 +5,47 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.tinderclone.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
+
+    private val firebaseAuth = FirebaseAuth.getInstance();
+    private val firebaseAuthListener = FirebaseAuth.AuthStateListener {
+        val user = firebaseAuth.currentUser
+        if(user != null){
+            startActivity(MainActivity.newIntent(this@SignupActivity))
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
     }
 
-    fun onSignup(view: View){
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener(firebaseAuthListener)
+    }
 
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
+    }
+
+    fun onSignup(view: View){
+        if(!edtEmail.text.toString().isNullOrEmpty() && !edtPassword.text.toString().isNullOrEmpty()){
+            firebaseAuth.createUserWithEmailAndPassword(edtEmail.text.toString(), edtPassword.text.toString())
+                .addOnCompleteListener { task ->
+                    if(!task.isSuccessful){
+                        Toast.makeText(this@SignupActivity, "Signup error ${task.exception?.localizedMessage}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 
     companion object{
